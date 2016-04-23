aa8d827e06c1a47a6340f8e30131a910ba76e750
rchau05 <rnchau@mail.sfsu.edu>
2016-04-23 01:33:01 -0700

    implemented factories and the ability to add comments

--- app.js
+++ app.js
@@ -1,40 +1,76 @@
 var app = angular.module('greenSpace', ['ui.router']);
 
-app.factory("posts", [function() {
-	var o = {
-		posts: [{title:"Welcome to my posts!", links:"", upvotes:0}]
-	};
-	return o;
-}]);
-
 app.controller('MainCtrl', [
 '$scope',
 'posts',
-	function($scope, posts){
-	  $scope.test = 'Hello world!';
-	  $scope.posts = posts.posts;
-
-	$scope.addPost = function() {
-		if(!$scope.title || $scope.title === "") {
-			alert("Please input something!")
-			return;
+function ($scope, posts){
+  $scope.test = 'Hello world!';
+  $scope.posts = posts.posts;
+	$scope.addPost = function(){
+		if(!$scope.title || $scope.title === '') { 
+			alert("Please enter a post!")
+			return; 
 		}
-		$scope.posts.push({
-			title: $scope.title,
-			link: $scope.link,
-			upvotes: 0
-		});
-		$scope.title = "";
-		$scope.link = ""
-	}
+  	$scope.posts.push({
+  		title: $scope.title,
+  		link: $scope.link, 
+  		upvotes: 0,
+  		comments: [
+    		{author: 'Joe', body: 'Cool post!', upvotes: 0},
+    		{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  		]
+  	});
+	  $scope.title = '';
+	  $scope.link ='';
+	};
 
 	$scope.incrementUpvotes = function(post) {
 		post.upvotes += 1;
-	}
-
-	// $scope.decrementUpvotes = function(post) {
-	// 	post.upvotes -= 1;
-	// }
+	};
 
 }]);
 
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+	$scope.post = posts.posts[$stateParams.id];
+	$scope.addComment = function(){
+	  if($scope.body === '') { return; }
+	  $scope.post.comments.push({
+	    body: $scope.body,
+	    author: 'user',
+	    upvotes: 0
+	  });
+	  $scope.body = '';
+	};
+}]);
+
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+		  url: '/posts/{id}',
+		  templateUrl: '/posts.html',
+		  controller: 'PostsCtrl'
+		});
+
+  $urlRouterProvider.otherwise('home');
+}]);
